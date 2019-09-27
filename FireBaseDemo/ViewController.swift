728f23322f3534409cf8f2eb4981a84d9d37e88c
SRS <srs@srss-macbook-pro.local>
2019-09-27 16:29:03 +0530

    Initial Commit

---
+++ FireBaseDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FireBaseDemo
+//
+//  Created by SRS on 27/09/19.
+//  Copyright © 2019 SRS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
